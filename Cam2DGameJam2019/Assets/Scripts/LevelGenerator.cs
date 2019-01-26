52c37042778b37fea2663400b98afe00fe7fa024
Kristian-Alekzandar Bakov <kristian.bakov@yahoo.com>
2019-01-26 13:05:13 +0000

    Start project

---
+++ Cam2DGameJam2019/Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+	public Texture2D map;
+
+	public ColourtoPrefab[] colourMappings;
+	
+	// Use this for initialization
+	void Start () 
+	{
+		GenerateLevel();
+	}
+
+	void GenerateLevel()
+	{
+		for (int x = 0; x < map.width; x++)
+		{
+			for (int y = 0; y < map.height; y++)
+			{
+				GenerateTile(x,y);
+			}
+		}
+	}
+
+	void GenerateTile(int x, int y)
+	{
+		Color pixelColour = map.GetPixel(x,y);
+
+		// If the pixel is of a transparent colour.
+		if(pixelColour.a == 0)
+		{
+			return;
+		}
+
+		foreach (ColourtoPrefab colourMapping in colourMappings)
+		{
+			if (colourMapping.colour.Equals(pixelColour))
+			{
+				Vector2 position = new Vector2(x,y);
+				Instantiate(colourMapping.prefab, position, Quaternion.identity, transform);
+			}
+		}
+		
+		// Debug.Log(pixelColour);
+
+		// Debug.Log("Hello there!");
+	}
+}
---
+++ Cam2DGameJam2019/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+
+	Animator character_animator;
+
+	float horizontal_move = 0f;
+
+	public float run_speed = 40f;
+
+	bool jump = false;
+	bool crouch = false;
+
+	public Collider2D player_collider;
+	public Collider2D object_collider;
+
+	void Start ()
+	{
+		character_animator = GetComponent<Animator>();
+
+		player_collider = GameObject.FindGameObjectWithTag("MainPlayer").GetComponent<Collider2D>();
+		object_collider = GameObject.FindGameObjectWithTag("MovableBox").GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{	
+		horizontal_move = Input.GetAxisRaw("Horizontal") * run_speed;
+
+		character_animator.SetBool("Move", false);
+		character_animator.SetBool("Push", false);
+		character_animator.SetBool("Jump", false);
+
+		PlayerBoxCollision();
+
+		if(Input.GetKey("d") || Input.GetKey("a"))
+		{
+			character_animator.SetBool("Move", true);
+		}
+
+		if(Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+
+			character_animator.SetBool("Jump", true);
+		}
+
+		if(Input.GetButtonDown("Crouch"))
+		{
+			crouch = true;
+		}
+		else if(Input.GetButtonUp("Crouch"))
+		{
+			crouch = false;
+		}
+	}
+
+	void FixedUpdate ()
+	{
+		controller.Move(horizontal_move * Time.fixedDeltaTime, crouch, jump);
+		jump = false;
+	}
+
+	void PlayerBoxCollision()
+	{
+		if(player_collider.IsTouching(object_collider))
+		{
+			character_animator.SetBool("Push", true);
+		}
+	}
+}
