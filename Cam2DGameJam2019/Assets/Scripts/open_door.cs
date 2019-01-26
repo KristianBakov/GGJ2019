8ee48a64d79a0af80d8880e80d4433bef1dfd1c2
Jacob Carr <jcarr343@hotmail.co.uk>
2019-01-26 16:26:29 +0000

    Added a door with a button

---
+++ Cam2DGameJam2019/Assets/Scripts/open_door.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class open_door : MonoBehaviour {
+
+    public GameObject open;
+    public GameObject closed;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        open.SetActive(true);
+        closed.SetActive(false);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        open.SetActive(false);
+        closed.SetActive(true);
+    }
+}
